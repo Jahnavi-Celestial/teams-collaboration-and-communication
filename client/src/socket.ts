a7f28489849209a8b012db5efc127af54e11bbd6
Jahnavi-Celestial <jahnavi.gaur@celestialsys.com>
2026-06-05 21:25:53 +0530

    fix socket connection url in frontend

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("import.meta.env.VITE_WS_URL", {
+export const socket = io(import.meta.env.VITE_WS_URL, {
   path: "/socket.io/",
   autoConnect: false,
   auth: (cb) => {
