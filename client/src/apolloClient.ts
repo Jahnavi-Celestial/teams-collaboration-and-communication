72bd1e37a623658e332f14cd7c2f0296391b11d6
jahnavi-celestial <jahnavi.gaur@celestialsys.com>
2026-06-06 00:10:37 +0530

    change apolloClient setup

--- client/src/apolloClient.ts
+++ client/src/apolloClient.ts
@@ -1,23 +1,25 @@
-import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+import { ApolloClient, InMemoryCache, createHttpLink, ApolloLink } from "@apollo/client";
 
 const httpLink = createHttpLink({
   uri: import.meta.env.VITE_BACKEND_URL,
 });
 
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('token');
-  return {
+const authLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem("token");
+  
+  operation.setContext({
     headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
-  };
+  });
+  
+  return forward(operation);
 });
 
 export const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache({
+    resultCaching: false,
     typePolicies: {
       Query: {
         fields: {
@@ -28,4 +30,13 @@ export const client = new ApolloClient({
       },
     },
   }),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: "network-only",
+      nextFetchPolicy: "network-only",
+    },
+    query: {
+      fetchPolicy: "network-only",
+    },
+  },
 });
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,38 +5,6 @@ import { AuthProvider } from "./context/AuthContext.tsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { ChatProvider } from "./context/ChatContext.tsx";
 
-if (typeof window !== "undefined") {
-  const originalFocus = HTMLInputElement.prototype.focus;
-  HTMLInputElement.prototype.focus = function (options) {
-    try {
-      if (this !== null && this !== undefined) {
-        originalFocus.call(this, options);
-      }
-    } catch (e) {
-      return 
-    }
-  };
-
-  const originalRemoveAttribute = Element.prototype.removeAttribute;
-  Element.prototype.removeAttribute = function (name: string) {
-    try {
-      if (this !== null && this !== undefined) {
-        originalRemoveAttribute.call(this, name);
-      }
-    } catch (e) {
-      return 
-    }
-  };
-
-  const originalSetAttribute = Element.prototype.setAttribute;
-  Element.prototype.setAttribute = function (name: string, value: string) {
-    if (name === "aria-hidden" && value === "true" && (this.id === "root" || this.tagName === "BODY")) {
-      return; 
-    }
-    return originalSetAttribute.call(this, name, value);
-  };
-}
-
 createRoot(document.getElementById("root")!).render(
   <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
     <AuthProvider>
